c2321ac7e0526588b376f472dca575b3584025a9
serge <serge.hall.dev@gmail.com>
2025-01-18 01:22:38 -0800

    remove double Header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -35,28 +35,3 @@ const Header: React.FC<HeaderProps> = ({ title, logoUrl, homeUrl }) => {
 };
 
 export default Header;
-
-
-// const Header: React.FC<HeaderProps> = ({ title, logoUrl, homeUrl }) => {
-//     const [isDropdownVisible, setIsDropdownVisible] = useState(false);
-//
-//     return (
-//         <HeaderContainer>
-//             {/* Ensure HeaderLink only wraps logo and title */}
-//             <HeaderLink href={homeUrl} aria-label="Home">
-//                 <LogoContainer>
-//                     <Logo src={logoUrl} alt="Site logo" />
-//                 </LogoContainer>
-//                 <Title>{title}</Title>
-//             </HeaderLink>
-//
-//             {/* Place Authorization separately */}
-//             <Authorization
-//                 isDropdownVisible={isDropdownVisible}
-//                 setIsDropdownVisible={setIsDropdownVisible}
-//             />
-//         </HeaderContainer>
-//     );
-// };
-//
-// export default Header;
