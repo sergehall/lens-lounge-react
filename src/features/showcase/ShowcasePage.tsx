b174aaa16fd8ffd9e4ec8003e1a687849c5a63f9
serge <serge.hall.dev@gmail.com>
2025-04-29 00:15:07 -0700

    - Removed DEFAULT_PROFILE loading from profileSlice - DEFAULT_PROFILE is now fully managed inside authSlice - Updated ShowcasePage, MyCategories, and Profile components to read profile only from authSlice - Secured all profile usage with null-checks before accessing profile fields - Simplified Redux store: profile state now lives only in auth slice

--- src/features/showcase/ShowcasePage.tsx
+++ src/features/showcase/ShowcasePage.tsx
@@ -1,6 +1,6 @@
 // src/features/showcase/ShowcasePage.tsx
-import React, {useEffect} from "react";
-import {useDispatch, useSelector} from "react-redux";
+import React from "react";
+import { useSelector} from "react-redux";
 import Profile from "./profile/Profile";
 import {
     BlogsSection,
@@ -55,7 +55,7 @@ const ShowcasePage: React.FC = () => {
                 )}
             </ProfileSection>
 
-            {/* ✅ Only show blog section if user is authenticated */}
+            {/* Only show blog section if user is authenticated */}
             {isAuthenticated && (
                 <BlogsSection>
                     <MyCategories />
@@ -66,76 +66,3 @@ const ShowcasePage: React.FC = () => {
 };
 
 export default ShowcasePage;
-
-
-
-
-
-
-// // src/features/showcase/ShowcasePage.tsx
-// import React, { useEffect } from "react";
-// import { useSelector, useDispatch } from "react-redux";
-// import Profile from "./profile/Profile";
-// import {
-//     BlogsSection,
-//     ProfileSection,
-//     ShowcaseContainer,
-//     PlaceholderProfileContainer,
-//     ActionButtonsWrapper,
-//     ActionButton,
-//     Divider,
-//     DividerText,
-// } from "./showcasePage.styles";
-// import { RootState, AppDispatch } from "../../app/store";
-// import { ProfileState, loadProfile } from "./profile/profileSlice";
-// import MyCategories from "./categories/MyCategories";
-//
-// const ShowcasePage: React.FC = () => {
-//     const dispatch = useDispatch<AppDispatch>();
-//
-//     const profile = useSelector((state: RootState) => state.showcasePage.profile) as ProfileState;
-//
-//     const isRegistered = Boolean(profile.firstName && profile.lastName && profile.photoUrl);
-//
-//     useEffect(() => {
-//         dispatch(loadProfile());
-//     }, [dispatch]);
-//
-//     const handleSignInClick = () => {
-//         console.log("Navigating to Sign In...");
-//     };
-//
-//     const handleCreateAccountClick = () => {
-//         console.log("Navigating to Create Account...");
-//     };
-//
-//     return (
-//         <ShowcaseContainer>
-//             <ProfileSection>
-//                 {isRegistered ? (
-//                     <Profile />
-//                 ) : (
-//                     <PlaceholderProfileContainer>
-//                         <h2>No Profile Found</h2>
-//                         <p>Please register or sign in to complete your profile and join the community!</p>
-//                         <Divider />
-//                         <ActionButtonsWrapper>
-//                             <ActionButton onClick={handleSignInClick}>SIGN IN</ActionButton>
-//                             <DividerText><span>or</span></DividerText>
-//                             <ActionButton onClick={handleCreateAccountClick}>CREATE ACCOUNT</ActionButton>
-//                         </ActionButtonsWrapper>
-//                     </PlaceholderProfileContainer>
-//                 )}
-//             </ProfileSection>
-//
-//             {/* Only render blogs if profile is complete */}
-//             {isRegistered && (
-//                 <BlogsSection>
-//                     <MyCategories />
-//                 </BlogsSection>
-//             )}
-//         </ShowcaseContainer>
-//     );
-// };
-//
-// export default ShowcasePage;
